b89f9cfeb69d1d8d81d7b88fc1ca3913f8f7cfd3
Filipe Pontes <106703491+filipetpontes@users.noreply.github.com>
2025-01-24 15:10:18 -0300

    Add files via upload

--- tempos_execucao.py
+++ tempos_execucao.py
@@ -11,10 +11,10 @@ def vi_regras_to_df(vi_regras):
 def duracoes_vi_regras(vi_regras):
     df = vi_regras_to_df(vi_regras)
     df_ok = df.loc[:, [0, 2, 3]]
-    df_ok.columns = ['FLUXO', 'INICIO', 'FIM']
-    df_ok['INICIO'] = pd.to_datetime(df_ok['INICIO'], format='%d/%m/%Y %H:%M:%S.%f')
-    df_ok['FIM'] = pd.to_datetime(df_ok['FIM'], format='%d/%m/%Y %H:%M:%S.%f')
-    df_ok['DURACAO'] = (df_ok['FIM'] - df_ok['INICIO']).dt.total_seconds() * 1000
+    df_ok.columns = ['Fluxo/Regra', 'Início', 'Fim']
+    df_ok['Início'] = pd.to_datetime(df_ok['Início'], format='%d/%m/%Y %H:%M:%S.%f')
+    df_ok['Fim'] = pd.to_datetime(df_ok['Fim'], format='%d/%m/%Y %H:%M:%S.%f')
+    df_ok['Duração (ms)'] = (df_ok['Fim'] - df_ok['Início']).dt.total_seconds() * 1000
     return df_ok
 
 def to_excel(df):
@@ -25,19 +25,18 @@ def to_excel(df):
     return processed_data
 
 def main():
-    st.title("NeuroAnáliseVIRegras")
+    st.title("Tempos de Execução")
 
     # Campos de entrada de texto
-    vi_regras = st.text_area("Proposta 1", placeholder="Cole aqui a VI_REGRAS...", height=100)
+    vi_regras = st.text_area("Proposta", placeholder="Cole aqui a VI_REGRAS...", height=100)
 
     # Botão para processar os dados
-    if st.button("Gerar Tabela"):
+    if st.button("Processar"):
         if vi_regras:
             # Processa os dados e gera o DataFrame
             df = duracoes_vi_regras(vi_regras)
 
             # Exibe a tabela na interface
-            st.write("### Resultado da Comparação")
             st.dataframe(df)
 
             # Converte o DataFrame para Excel
@@ -47,7 +46,7 @@ def main():
             st.download_button(
                 label="Baixar Excel",
                 data=excel_file,
-                file_name="comparativo.xlsx",
+                file_name="tempos_execucao.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
         else:
